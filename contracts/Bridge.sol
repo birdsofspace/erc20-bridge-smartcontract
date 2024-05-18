66fd2e6db2bebfaa9fd70dbdba099a99b0cc1936
Birds of Space <162083581+birdsofspace@users.noreply.github.com>
2024-05-19 00:34:26 +0700

    Update Bridge.sol

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -6,10 +6,42 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
+library StringsUtil {
+    function toLower(string memory str) internal pure returns (string memory) {
+        bytes memory bStr = bytes(str);
+        bytes memory bLower = new bytes(bStr.length);
+        
+        for (uint i = 0; i < bStr.length; i++) {
+            // Mengubah huruf besar (A-Z) menjadi huruf kecil (a-z)
+            if ((bStr[i] >= 0x41) && (bStr[i] <= 0x5A)) {
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        
+        return string(bLower);
+    }
+
+     function toHexString(bytes32 data) internal pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(64);
+        
+        for (uint i = 0; i < 32; i++) {
+            str[i*2] = alphabet[uint(uint8(data[i] >> 4))];
+            str[1+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
+        }
+        
+        return string(str);
+    }
+}
+
 contract Bridge {
     using ECDSA for bytes32;
     using Address for address;
     using MessageHashUtils for bytes32;
+    using StringsUtil for string;
+    using StringsUtil for bytes32;
 
     IERC20Upgradeable public token;
     address public federationAddress;
@@ -91,9 +123,9 @@ contract Bridge {
             20
         );
 
-        string memory dataPack = string.concat(user_bridge, source_chainID, target_chainID, source_contract, target_contract, symbol, decimal, amount, sign_at);
+        string memory dataPack = string.concat("BRIDGEX-", user_bridge, source_chainID, target_chainID, source_contract, target_contract, symbol, decimal, amount, sign_at).toLower();
 
-        bytes32 transaction = keccak256(bytes(dataPack));
+        bytes32 transaction = keccak256(abi.encodePacked(dataPack));
 
         require(
             !usedSignatures[signature],
@@ -105,7 +137,7 @@ contract Bridge {
         );
         require(
             verifySignature(transaction, federationAddress, signature),
-            "Failed claim token!"
+             string.concat("Failed claim token: ", dataPack, "[",transaction.toHexString(),"]")
         );
 
         token = IERC20Upgradeable(stringToAddress(target_contract));
@@ -188,4 +220,5 @@ contract Bridge {
             revert(string(abi.encodePacked("Invalid byte value: ", _byte)));
         }
     }
+
 }
